d7af7d4e5e9c0e0ba7e19eff8e7adfcf1671d2d4
ajauni01 <ajauni01@wsc.edu>
2025-03-02 05:07:49 -0600

    Get all the jobs data

--- frontend/src/components/Pages/Inbox.jsx
+++ frontend/src/components/Pages/Inbox.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { FaSpinner, FaInbox, FaCheck, FaPaperPlane } from "react-icons/fa";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // Connect to the backend
+const socket = io("http://localhost:3000"); // Connect to the backend
 
 const Inbox = ({ user }) => {
   const [messages, setMessages] = useState([]);
